4373518f1bfefe2a7ad16744f33e0829a194ee9b
Adheesha <adheeshadissanayake001@gmail.com>
2026-06-07 20:01:39 +0530

    Student input functionality added

--- Main.java
+++ Main.java
@@ -1,22 +1,23 @@
+import java.util.Scanner;
 class Student{
-    String studentID;
+    int studentID;
     String studentName;
     int marks;
-    public Student(String studentID,String studentName,int marks){
+    public Student(int studentID,String studentName,int marks){
         this.studentID = studentID;
         this.studentName = studentName;
         this.marks = marks;
     }
-    public String getStudentID(){
+    public int getStudentID(){
         return studentID;
     }
-    public String getSudentName(){
+    public String getStudentName(){
         return studentName;
     }
-    public int getMakrs(){
+    public int getMarks(){
         return marks;
     }
-    public void setStudentID(String studentID){
+    public void setStudentID(int studentID){
         this.studentID = studentID;
     }
     public void setStudentName(String studentName){
@@ -25,8 +26,26 @@ class Student{
     public void setMarks(int marks){
         this.marks = marks;
     }
+	
 }
 public class Main {
     public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of entries to be added");
+		int size = sc.nextInt();
+		Student[] arr = new Student[size];
+		for(int i=0; i < size; i++){
+			System.out.println("Enter the Student ID: ");
+			int ID = sc.nextInt();
+			sc.nextLine();
+			System.out.println("Enter the Student Name: ");
+			String name = sc.nextLine();
+			System.out.println("Enter the Student's Marks: ");
+			int marks = sc.nextInt();
+			sc.nextLine();
+			arr[i] = new Student(ID,name,marks);
+		}
+		
+		sc.close();
     }
 }
